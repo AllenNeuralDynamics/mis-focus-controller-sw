c9fadf3eec4710d554341ae1275b2f999f6e2a17
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-01-05 16:01:40 -0800

    demo.py: remove cruft

--- examples/demo.py
+++ examples/demo.py
@@ -14,17 +14,8 @@ if __name__ == "__main__":
     print("done.")
 
     on_time = 3.0
-    #focus_rig.set_speed(MOTOR_INDEX, 30)
-    #print(f"Moving motor {MOTOR_INDEX} forward for {on_time} [sec].")
-    #focus_rig.time_move(MOTOR_INDEX, True, round(on_time * 1000), wait=True)
-    #sleep(0.1)
-    #print(f"Moving motor {MOTOR_INDEX} backward for {on_time} [sec].")
-    #focus_rig.time_move(MOTOR_INDEX, False, round(on_time * 1000), wait=True)
-    #print("End of demo.")
-
 
     for i in range(3):
-        focus_rig.set_speed(i, 30)
         focus_rig.set_speed(i, 30)
         print(f"Moving motor {i} forward for {on_time} [sec].")
         focus_rig.time_move(i, True, round(on_time * 1000), wait=True)
